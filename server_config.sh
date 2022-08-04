9afdb518f5d43777269fb45980932b7ca3921492
Mat-helper <maticzhelper@gmail.com>
2022-08-04 19:29:25 +0530

    add the cache clear file and mongo backup script file

--- server_config.sh
+++ server_config.sh
@@ -22,6 +22,8 @@ fi
 cd ${ROOTDIR}
 
 export PKG_DIR="${ROOTDIR}/pkg"
+export CONFIG_DIR="${ROOTDIR}/conf"
+export TOOLS_DIR="${ROOTDIR}/tools"
 export RUNTIME_DIR="${ROOTDIR}/runtime"
 
 [[ -d ${RUNTIME_DIR} ]] || mkdir -p ${RUNTIME_DIR}
@@ -31,60 +33,26 @@ export INSTALL_LOG="${RUNTIME_DIR}/install.log"
 export PKG_INSTALL_LOG="${RUNTIME_DIR}/pkg.install.log"
 
 . ${PKG_DIR}/installation
+. ${CONFIG_DIR}/apache2
+
+
+# root user/group name. Note: not all OSes have group 'root'.
+export SYS_USER_ROOT='root'
+export SYS_GROUP_ROOT='root'
+
+# Backup script file names.
+export BACKUP_SCRIPT_MONGO_NAME='backup_mongo.sh'
+
+export BACKUP_DIR='/root'
+
+mongo_backup_script="${BACKUP_DIR}/${BACKUP_SCRIPT_MONGO_NAME}"
+
+    ECHO_INFO "Setup daily cron job to backup SQL databases with ${mongo_backup_script}"
+
+    [ ! -d ${BACKUP_DIR} ] && mkdir -p ${BACKUP_DIR} >> ${INSTALL_LOG} 2>&1
+     backup_file ${mongo_backup_script}
+    cp ${TOOLS_DIR}/${BACKUP_SCRIPT_MONGO_NAME} ${mongo_backup_script}
+    chown ${SYS_USER_ROOT}:${SYS_GROUP_ROOT} ${mongo_backup_script}
+    chmod 0500 ${mongo_backup_script}
+
 ###########################################################################################
-#################################
-#apache server  configuration
-#################################
-
-# allow override none to all for /var/www
-sudo sed -i -e '172 s/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf
-
-#configure X-frame-options
-sudo echo -e "Header set X-Frame-Options: \"sameorigin\"" >> /etc/apache2/conf-available/security.conf
-
-# write the protocol for http2 
-sudo sed -i "3 i Protocols h2 http/1.1" /etc/apache2/sites-available/default-ssl.conf
-
-# http to https redirect write at virtual host
-sudo sed -i "4 i Redirect permanent / https://${DOMAIN_NAME}/" /etc/apache2/sites-available/000-default.conf
-
-#ssl file create at location /var/www/sslkeys
-sudo mkdir -p /var/www/sslkeys
-
-touch /var/www/sslkeys/${CERT_FILE}.csr /var/www/sslkeys/${CERT_FILE}.key /var/www/sslkeys/${CERT_FILE}.ca-bundle
-
-#comment the default ssl file path line at default-ssl.conf
-sudo sed -i -e '33 s/SSLCertificateFile/#SSLCertificateFile/g' /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i -e '34 s/SSLCertificateKeyFile/#SSLCertificateKeyFile/g' /etc/apache2/sites-available/default-ssl.conf
-
-#insert the ssl certificate , private key , ca-bundle 
-sudo sed -i "35 i SSLCertificateFile /var/www/sslkeys/${CERT_FILE}.csr" /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i "36 i SSLCertificateKeyFile /var/www/sslkeys/${CERT_FILE}.key" /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i "37 i SSLCertificateChainFile /var/www/sslkeys/${CERT_FILE}.ca-bundle" /etc/apache2/sites-available/default-ssl.conf
-
-#servername & server alias name 
-sudo sed -i "2 i ServerName ${DOMAIN_NAME}" /etc/apache2/sites-available/000-default.conf
-sudo sed -i "3 i ServerAlias ${DOMAIN_ALIAS_NAME}" /etc/apache2/sites-available/000-default.conf
-sudo sed -i "5 i ServerName ${DOMAIN_NAME}" /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i "6 i ServerAlias ${DOMAIN_ALIAS_NAME}" /etc/apache2/sites-available/default-ssl.conf
-
-#add the proxy name
-sudo sed -i '135 i <Proxy *>\nOrder deny,allow\nAllow from all' /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i '138 i </Proxy>\nSSLProxyEngine On\nProxyRequests Off\nProxyPreserveHost On' /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i '142 i ProxyPass / http://127.0.0.1:2053/\nProxyPassReverse / http://127.0.0.1:2053/' /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i '144 i ProxyPass / http://127.0.0.1:2053/v1\nProxyPassReverse / http://127.0.0.1:2053/v1' /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i '146 i ProxyPass / http://127.0.0.1:2053/images\nProxyPassReverse / http://127.0.0.1:2053/images' /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i '148 i ProxyPass / http://127.0.0.1:2053/nftImg\nProxyPassReverse / http://127.0.0.1:2053/nftImg' /etc/apache2/sites-available/default-ssl.conf
-sudo sed -i '150 i ProxyPass / http://127.0.0.1:2053/compressedImage\nProxyPassReverse / http://127.0.0.1:2053/compressedImage' /etc/apache2/sites-available/default-ssl.conf
-
-#enable the ssl-virtualhost file
-sudo a2ensite default-ssl.conf
-
-#enable http2 htaccess rewrite
-sudo a2enmod http2 headers rewrite ssl proxy proxy_ssl proxy_balancer 
-
-#test the configuration are completed with or without error
-sudo apache2ctl configtest
-
-#restart the server 
-sudo systemctl restart apache2
---
+++ tools/backup_mongo.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+KEEP_DAYS='20'
+
+export PATH='/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin'
+
+export CMD_DATE='/bin/date'
+export CMD_DU='du -sh'
+
+
+export TIME="$(${CMD_DATE} +%H-%M-%S)"
+export YEAR="$(${CMD_DATE} +%Y)"
+export MONTH="$(${CMD_DATE} +%m)"
+export DAY="$(${CMD_DATE} +%d)"
+export TIMESTAMP="${YEAR}-${MONTH}-${DAY}-${TIME}"
+
+export DB_BACKUP_PATH='/backup/mongo'
+export MONGO_HOST='13.56.100.141'
+export MONGO_PORT='9080'
+export MONGO_USER='Mverfg'
+export MONGO_PASSWD='Y8kbt478we9'
+export DATABASE_NAMES='matdb'
+
+# Pre-defined backup status
+export BACKUP_SUCCESS='YES'
+
+mkdir -p ${DB_BACKUP_PATH}/${YEAR}/${MONTH}/${DAY}/${TIME}
+
+# Log file
+export LOGFILE="${DB_BACKUP_PATH}/${YEAR}/${MONTH}/${DAY}/${TIME}/${TIMESTAMP}.log"
+
+# Initialize log file.
+echo "* Starting backup: ${TIMESTAMP}." >${LOGFILE}
+echo "* Backup directory: ${DB_BACKUP_PATH}/${YEAR}/${MONTH}/${DAY}/${TIME}." >>${LOGFILE}
+
+ 
+#if [ ${DATABASE_NAMES} = "ALL" ]; then
+# echo "You have choose to backup all databases: ${DATABASE_NAMES}" >>${LOGFILE}
+# mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} --username ${MONGO_USER} --password ${MONGO_PASSWD} --out ${DB_BACKUP_PATH}/${YEAR}/${MONTH}/${DAY}/${TIME}  &>/dev/null
+#else 
+if [ X"$?" == X'0' ]; then
+ echo "Running backup for selected databases: ${DATABASE_NAMES}" >>${LOGFILE}
+   for DB_NAME in ${DATABASE_NAMES}
+    do
+    mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} --db ${DB_NAME} --username ${MONGO_USER} --password ${MONGO_PASSWD} --out ${DB_BACKUP_PATH}/${YEAR}/${MONTH}/${DAY}/${TIME}  &>/dev/null
+   done
+else
+    # backup failed
+    export BACKUP_SUCCESS='NO'
+fi
+
+# Append file size of backup files.
+echo -e "* File size:\n----" >>${LOGFILE}
+cd ${DB_BACKUP_PATH}/${YEAR}/${MONTH}/${DAY}/${TIME} && \
+${CMD_DU} ${DATABASE_NAMES} >>${LOGFILE}
+echo "----" >>${LOGFILE}
+
+echo "* Backup completed (Success? ${BACKUP_SUCCESS})." >>${LOGFILE}
+
+if [ X"${BACKUP_SUCCESS}" == X'YES' ]; then
+    echo "==> Backup completed successfully."
+else
+    echo -e "==> Backup completed with !!!ERRORS!!!.\n" 1>&2
+fi
+
+# Find the old backup which should be removed.
+export REMOVE_OLD_BACKUP='YES'
+
+export KERNEL="$(uname -s)"
+if [[ X"${KERNEL}" == X'Linux' ]]; then
+    shift_year=$(date --date="${KEEP_DAYS} days ago" "+%Y")
+    shift_month=$(date --date="${KEEP_DAYS} days ago" "+%m")
+    shift_day=$(date --date="${KEEP_DAYS} days ago" "+%d")
+elif [[ X"${KERNEL}" == X'FreeBSD' ]]; then
+    shift_year=$(date -j -v-${KEEP_DAYS}d "+%Y")
+    shift_month=$(date -j -v-${KEEP_DAYS}d "+%m")
+    shift_day=$(date -j -v-${KEEP_DAYS}d "+%d")
+elif [[ X"${KERNEL}" == X'OpenBSD' ]]; then
+    epoch_seconds_now="$(date +%s)"
+    epoch_shift="$((${KEEP_DAYS} * 86400))"
+    epoch_seconds_old="$((epoch_seconds_now - epoch_shift))"
+
+    shift_year=$(date -r ${epoch_seconds_old} "+%Y")
+    shift_month=$(date -r ${epoch_seconds_old} "+%m")
+    shift_day=$(date -r ${epoch_seconds_old} "+%d")
+else
+    export REMOVE_OLD_BACKUP='NO' 
+fi
+
+export REMOVED_BACKUP_DIR="${DB_BACKUP_PATH}/${shift_year}/${shift_month}/${shift_day}"
+export REMOVED_BACKUP_MONTH_DIR="${DB_BACKUP_PATH}/${shift_year}/${shift_month}"
+export REMOVED_BACKUP_YEAR_DIR="${DB_BACKUP_PATH}/${shift_year}"
+
+
+if [[ X"${REMOVE_OLD_BACKUP}" == X'YES' ]] && [[ -d "${REMOVED_BACKUP_DIR}" ]]; then
+    echo -e "* Old backup found. Deleting: ${REMOVED_BACKUP_DIR}." >>${LOGFILE}
+    rm -rf ${REMOVED_BACKUP_DIR} 
+
+    # Try to remove empty directory.
+    rmdir ${REMOVED_BACKUP_MONTH_DIR} 2>/dev/null
+    rmdir ${REMOVED_BACKUP_YEAR_DIR} 2>/dev/null
+else
+    echo -e "Not Found Old backup to delete." >>${LOGFILE}
+fi
+
+echo "==> Detailed log (${LOGFILE}):"
+echo "========================="
+cat ${LOGFILE}
