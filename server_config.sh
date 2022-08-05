897598af9acf86105bd5a7965b189d00f8716200
Mat-helper <maticzhelper@gmail.com>
2022-08-05 11:13:11 +0530

    create the global file for variable and functions

--- server_config.sh
+++ server_config.sh
@@ -8,9 +8,10 @@ export DOMAIN_ALIAS_NAME='www.example.com'
 export CERT_FILE=${DOMAIN_NAME//./_}
 
 #############################################################################
-################################
-#runtime directory creation and name for the log files
-################################
+# ------------------------------
+# Define some global variables.
+# ------------------------------
+
 tmprootdir="$(dirname $0)"
 echo ${tmprootdir} | grep '^/' >/dev/null 2>&1
 if [ X"$?" == X"0" ]; then
@@ -28,56 +29,19 @@ export RUNTIME_DIR="${ROOTDIR}/runtime"
 
 [[ -d ${RUNTIME_DIR} ]] || mkdir -p ${RUNTIME_DIR}
 
-export STATUS_FILE="${RUNTIME_DIR}/install.status"
-export INSTALL_LOG="${RUNTIME_DIR}/install.log"
-export PKG_INSTALL_LOG="${RUNTIME_DIR}/pkg.install.log"
 
+. ${CONF_DIR}/global
+. ${CONF_DIR}/core
 . ${PKG_DIR}/installation
 . ${CONFIG_DIR}/apache2
 
-
-
-# Output flag.
-export _INFO_FLAG="[ INFO ]"
+chmod go+rx /dev/null /dev/*random &>/dev/null
 
 # Debug mode: YES, NO.
 export SERVER_DEBUG="${SERVER_DEBUG:=NO}"
 
-# root user/group name. Note: not all OSes have group 'root'.
-export SYS_USER_ROOT='root'
-export SYS_GROUP_ROOT='root'
 
-
-ECHO_INFO()
-{
-    if [ X"$1" == X"-n" ]; then
-        shift 1
-        echo -ne "${_INFO_FLAG} $@"
-    else
-        echo -e "${_INFO_FLAG} $@"
-    fi
-    echo -e "${_INFO_FLAG} $@" >> ${INSTALL_LOG}
-}
-
-backup_file()
-{
-    # Usage: backup_file file1 [file2 file3 ... fileN]
-    if [ X"$#" != X"0" ]; then
-        for f in $@; do
-            if [ -f ${f} ]; then
-                if [ X"${SERVER_DEBUG}" == X'YES' ]; then
-                    echo -e "${_BACKUP_FLAG} ${f} -> ${f}.${DATE}."
-                fi
-                cp -f ${f} ${f}.${DATE}
-            fi
-        done
-    fi
-}
-
-# Backup script file names.
-export BACKUP_SCRIPT_MONGO_NAME='backup_mongo.sh'
-
-export BACKUP_DIR='/root'
+export BACKUP_DIR='/root/script'
 
 mongo_backup_script="${BACKUP_DIR}/${BACKUP_SCRIPT_MONGO_NAME}"
 
@@ -89,4 +53,26 @@ mongo_backup_script="${BACKUP_DIR}/${BACKUP_SCRIPT_MONGO_NAME}"
     chown ${SYS_USER_ROOT}:${SYS_GROUP_ROOT} ${mongo_backup_script}
     chmod 0500 ${mongo_backup_script}
 
-###########################################################################################
+  # Add cron job
+    cat >> ${CRON_FILE_ROOT} <<EOF
+# Backup MONGO databases at minute 0 past every 2nd hour
+0   */2   *   *   *  ${SHELL_BASH} ${mongo_backup_script}
+
+EOF
+
+
+cache_clear_script="${BACKUP_DIR}/${BUFFER_CACHE_CLEAR}"
+
+    ECHO_INFO "Setup daily cron job to Cache clear script ${cache_clear_script}"
+
+    [ ! -d ${BACKUP_DIR} ] && mkdir -p ${BACKUP_DIR} >> ${INSTALL_LOG} 2>&1
+    cp ${TOOLS_DIR}/${BUFFER_CACHE_CLEAR} ${cache_clear_script}
+    chown ${SYS_USER_ROOT}:${SYS_GROUP_ROOT} ${cache_clear_script}
+    chmod 0500 ${cache_clear_script}
+
+  # Add cron job
+    cat >> ${CRON_FILE_ROOT} <<EOF
+# Backup MONGO databases at minute 0 past every 8th hour
+0 */8 * * *  ${SHELL_BASH} ${cache_clear_script}
+
+EOF
