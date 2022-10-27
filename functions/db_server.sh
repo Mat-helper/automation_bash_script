bb1fb1ef5a5cde32dd4af25d7aba4a256f632e47
Mat-helper <109207613+Mat-helper@users.noreply.github.com>
2022-10-27 12:51:52 +0530

    alter the variable name
    remove the variable name confusion for mongo db user creation

--- functions/db_server.sh
+++ functions/db_server.sh
@@ -8,13 +8,27 @@ db_server_config()
 
     if [ X"${DB_SERVER}" == X'MONGO' ]; then
        
-    # For Mongo database management.
+    # For Mongo database admin user.
     echo " Mongo accessing Admin details: " >> ${SERVER_CONFIG_FILE}
     export MONGO_DB_ADMIN_USER="$(${RANDOM_STRING})"
     echo "export MONGO_DB_ADMIN_USER='${MONGO_DB_ADMIN_USER}'" >> ${SERVER_CONFIG_FILE}
 
     export MONGO_DB_ADMIN_PASSWD="$(${RANDOM_STRING})"
     echo "export MONGO_DB_ADMIN_PASSWD='${MONGO_DB_ADMIN_PASSWD}'" >> ${SERVER_CONFIG_FILE}
+
+    # For Mongo database user details.
+    echo " Mongo accessing Admin details: " >> ${SERVER_CONFIG_FILE}
+    export MONGO_USER="$(${RANDOM_STRING})"
+    echo "export MONGO_USER='${MONGO_USER}'" >> ${SERVER_CONFIG_FILE}
+
+    export MONGO_PASSWD="$(${RANDOM_STRING})"
+    echo "export MONGO_PASSWD='${MONGO_PASSWD}'" >> ${SERVER_CONFIG_FILE}
+
+    export DATABASE_NAME="$(${RANDOM_STRING})"
+    echo "export DATABASE_NAME='${DATABASE_NAME}'" >> ${SERVER_CONFIG_FILE}
+
+    export MONGO_PORT="$(${RANDOM_NUMBER})"
+    echo "export MONGO_PORT='${MONGO_PORT}'" >> ${SERVER_CONFIG_FILE}
 
      check_status_before_run mongo_setup
     fi
