cd44953ed39f535663a52d84f9877c7742bb2738
Mat-helper <109207613+Mat-helper@users.noreply.github.com>
2022-10-31 18:18:59 +0530

    update the ssl configuration at apache2 server

--- functions/apache2.sh
+++ functions/apache2.sh
@@ -2,7 +2,7 @@
 
 apache2_config()
 {
-    ECHO_INFO "Configure Apache2 web server."
+    ECHO_INFO "Configure Apache2 web for http server."
 
     # Make sure we have an empty directory
     # Directory used to store virtual web hosts config files
@@ -12,18 +12,17 @@ apache2_config()
     [ -d ${HTTP_CONF_DIR_ENABLED_SITES} ] && mv ${HTTP_CONF_DIR_ENABLED_SITES} ${HTTP_CONF_DIR_ENABLED_SITES}.bak
     [ ! -d ${HTTP_CONF_DIR_ENABLED_SITES} ] && mkdir -p ${HTTP_CONF_DIR_ENABLED_SITES}
 
-    backup_file ${APACHE2_CONF} ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT} ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+    #backup the apache2, default conf file
+    backup_file ${APACHE2_CONF} ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT}
     #
     # Modular config files
     #
     # Copy sample files
     cp ${APACHE2_SAMPLE_DIR}/apache2.conf ${APACHE2_CONF}
 
-    #
     # Default sites
-    #
     cp -f ${APACHE2_SAMPLE_DIR}/sites-available/000-default.conf ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT}
-    cp -f ${APACHE2_SAMPLE_DIR}/sites-available/default-ssl.conf ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+    
     cd ${HTTP_CONF_DIR_AVAILABLE_SITES}
     
     #configure X-frame-options
@@ -31,10 +30,9 @@ apache2_config()
 
     # Ports
     perl -pi -e 's#PH_PORT_HTTP#$ENV{PORT_HTTP}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT}
-    perl -pi -e 's#PH_HTTPS_PORT#$ENV{HTTPS_PORT}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
 
     # web sites
-    perl -pi -e 's#PH_HTTP_DOCUMENTROOT#$ENV{HTTP_DOCUMENTROOT}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/*.conf
+    perl -pi -e 's#PH_HTTP_DOCUMENTROOT#$ENV{HTTP_DOCUMENTROOT}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT}
     
     # Domain & subdomain name
     
@@ -42,23 +40,22 @@ apache2_config()
 
     export FLQN_NAME="${SUBDOMAIN_NAME}.${DOMAIN_NAME}"
      
-    perl -pi -e 's#PH_DOMAIN_NAME#$ENV{DOMAIN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/*.conf
-    perl -pi -e 's#PH_SUBDOMAIN_NAME#$ENV{SUBDOMAIN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/*.conf
-    perl -pi -e 's#PH_FLQN_NAME#$ENV{FLQN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/*.conf
+    perl -pi -e 's#PH_DOMAIN_NAME#$ENV{DOMAIN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT}
+    perl -pi -e 's#PH_SUBDOMAIN_NAME#$ENV{SUBDOMAIN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT}
+    perl -pi -e 's#PH_FLQN_NAME#$ENV{FLQN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT}
     
     else 
 
     export FLQN_NAME="${SUBDOMAIN_NAME}.${DOMAIN_NAME}"
 
-    perl -pi -e 's#PH_DOMAIN_NAME#$ENV{FLQN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/*.conf
-    perl -pi -e 's#PH_FLQN_NAME#$ENV{FLQN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/*.conf
+    perl -pi -e 's#PH_DOMAIN_NAME#$ENV{FLQN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT}
+    perl -pi -e 's#PH_FLQN_NAME#$ENV{FLQN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT}
 
     sudo sed -i -e '11 s/ServerAlias/#ServerAlias/g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT}
-    sudo sed -i -e '6 s/ServerAlias/#ServerAlias/g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
     
     fi
     #enable http2 htaccess rewrite 
-    a2enmod http2 headers rewrite 
+   a2enmod headers rewrite 
     cd ${HTTP_CONF_DIR_AVAILABLE_SITES}
     ${SITE_ENABLE} ${APACHE2_CONF_SITE_DEFAULT} >> ${INSTALL_LOG} 2>&1
     cd ${ROOTDIR}
--- functions/ssl_configuration.sh
+++ functions/ssl_configuration.sh
@@ -2,7 +2,43 @@
 
 generate_ssl_keys ()
 {
+    
     if [ X"${ssl_configuration}" == X'SSLPURCHASED' ]; then
+
+    ECHO_INFO "Configure Apache2 web for https server."
+
+    #backup the default-ssl conf file
+    backup_file ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+
+    # Copy default-ssl sites
+    cp -f ${APACHE2_SAMPLE_DIR}/sites-available/default-ssl.conf ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+
+    # Ports
+    perl -pi -e 's#PH_HTTPS_PORT#$ENV{HTTPS_PORT}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+    
+    # web sites
+    perl -pi -e 's#PH_HTTP_DOCUMENTROOT#$ENV{HTTP_DOCUMENTROOT}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+    
+    # Domain & subdomain name
+    
+    if [ X"${SUBDOMAIN_NAME}" == X'www' ]; then
+
+    export FLQN_NAME="${SUBDOMAIN_NAME}.${DOMAIN_NAME}"
+     
+    perl -pi -e 's#PH_DOMAIN_NAME#$ENV{DOMAIN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+    perl -pi -e 's#PH_SUBDOMAIN_NAME#$ENV{SUBDOMAIN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+    perl -pi -e 's#PH_FLQN_NAME#$ENV{FLQN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+    
+    else 
+
+    export FLQN_NAME="${SUBDOMAIN_NAME}.${DOMAIN_NAME}"
+
+    perl -pi -e 's#PH_DOMAIN_NAME#$ENV{FLQN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+    perl -pi -e 's#PH_FLQN_NAME#$ENV{FLQN_NAME}#g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+
+    sudo sed -i -e '6 s/ServerAlias/#ServerAlias/g' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
+    
+    fi
 
         [[ -d  ${SSL_DIR} ]] || mkdir -p ${SSL_DIR}
 
@@ -12,15 +48,14 @@ generate_ssl_keys ()
 
          [[ -f ${SSL_FULLCHAIN_FILE} ]] || touch ${SSL_CERT_FILE} 
 
-        ECHO_INFO " Copy paste the SSL key file in repestive order ${SSL_CERT_FILE}, ${SSL_KEY_FILE}, ${SSL_FULLCHAIN_FILE}"
+        ECHO_INFO " Copy paste the SSL key file in repestive order ${SSL_CERT_FILE}"
         sleep 3
-
         nano ${SSL_CERT_FILE}
         sleep 2
-
+        ECHO_INFO " Copy paste the SSL key file in repestive order ${SSL_KEY_FILE}"
         nano ${SSL_KEY_FILE}
         sleep 2
-
+        ECHO_INFO " Copy paste the SSL key file in repestive order ${SSL_FULLCHAIN_FILE}"
         nano ${SSL_FULLCHAIN_FILE}
         sleep 2
 
@@ -33,8 +68,8 @@ generate_ssl_keys ()
         cd ${HTTP_CONF_DIR_AVAILABLE_SITES}
         ${SITE_ENABLE} ${APACHE2_CONF_SITE_DEFAULT_SSL} >> ${INSTALL_LOG} 2>&1
         cd ${ROOTDIR}
-        #enable http2 htaccess rewrite 
-        a2enmod ssl
+        #enable http2 ssl 
+        a2enmod http2 ssl
 
         # starting apache2
         ECHO_DEBUG "Restart service: ${APACHE2_RC_SCRIPT_NAME}."
