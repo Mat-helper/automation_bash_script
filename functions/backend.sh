42a8f1c138eda641c7f310c140e0d889d63e95e2
Mat-helper <109207613+Mat-helper@users.noreply.github.com>
2022-10-31 19:56:41 +0530

    Update backend.sh

--- functions/backend.sh
+++ functions/backend.sh
@@ -28,7 +28,7 @@ backend_install()
         sed -i -e '18,27 {s/#//g}' ${HTTP_CONF_DIR_AVAILABLE_SITES}/${APACHE2_CONF_SITE_DEFAULT_SSL}
 
         #enable http2 htaccess rewrite 
-         a2enmod proxy proxy_ssl proxy_balancer proxy_http proxy_http2 proxy_wstunnel
+         a2enmod proxy proxy_balancer proxy_http proxy_http2 proxy_wstunnel
 
         # starting apache2
          ECHO_INFO "Restart service: ${APACHE2_RC_SCRIPT_NAME}."
@@ -41,7 +41,7 @@ backend_install()
         sudo sed -i '25 i <Proxy *>\nOrder deny,allow\nAllow from all\n</Proxy>\nSSLProxyEngine On\nProxyRequests Off\nProxyPreserveHost On\nProxyPass / http://127.0.0.1:2053/\nProxyPassReverse / http://127.0.0.1:2053/\n' ${HTTP_CONF_DIR_AVAILABLE_SITES}/000-default-le-ssl.conf
 
         #enable http2 htaccess rewrite 
-         a2enmod proxy proxy_ssl proxy_balancer proxy_http proxy_http2 proxy_wstunnel
+         a2enmod proxy proxy_balancer proxy_http proxy_http2 proxy_wstunnel
 
         # starting apache2
          ECHO_INFO "Restart service: ${APACHE2_RC_SCRIPT_NAME}."
