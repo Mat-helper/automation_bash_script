4bcdefd50ccba2080c9ea450a93d4f01c940a3b6
Maticz-Technology <projectmanager@maticz.com>
2023-10-09 17:26:43 +0530

    update the landing page at server

--- functions/web_server.sh
+++ functions/web_server.sh
@@ -5,6 +5,8 @@ web_server_config()
     # Create required directories
     [ -d ${HTTP_DOCUMENTROOT} ] || mkdir -p ${HTTP_DOCUMENTROOT} >> ${INSTALL_LOG} 2>&1
 
+    cp -f ${APACHE2_SAMPLE_DIR}/index.html ${HTTP_DOCUMENTROOT}
+
     if [ X"${WEB_SERVER}" == X'APACHE2' ]; then
         . ${FUNCTIONS_DIR}/apache2.sh
         check_status_before_run apache2_config
